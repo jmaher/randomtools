ffd5e24bc494fa114dc44edef6da91a5fd995070
Joel Maher <joel.maher@gmail.com>
2015-07-15 18:10:47 +0100

    more complete this time, with fuzzy bits

--- comparealerts/parse_alerts.py
+++ comparealerts/parse_alerts.py
@@ -2,12 +2,108 @@ import csv
 import time
 from talosmaps import TBPL_TESTS, PLATFORMS
 import datetime
+import requests
+import os
+import json
+import copy
+
+revision_cache = {}
+pushlog_cache = {}
+
+def loadRevisionCache():
+    global revision_cache
+    if os.path.exists('revisions.cache'):
+        try:
+            with open('revisions.cache', 'rb') as fHandle:
+                revision_cache = json.load(fHandle)
+        except:
+            print "FAILED TO LOAD REVISION CACHE"
+            revision_cache = {}
+
+def saveRevisionCache():
+    global revision_cache
+    with open('revisions.cache', 'wb') as fHandle:
+        json.dump(revision_cache, fHandle)
+
+
+def loadPushLogCache():
+    global pushlog_cache
+    if os.path.exists('pushlog.cache'):
+        try:
+            with open('pushlog.cache', 'rb') as fHandle:
+                pushlog_cache = json.load(fHandle)
+        except:
+            pushlog_cache = {}
+
+    if not pushlog_cache:
+        for branch in ['integration/mozilla-inbound',
+                       'integration/fx-team',
+                       'integration/b2g-inbound',
+                       'mozilla-central']:
+            pushlog_cache[branch] = []
+
+            dateArgs = "startdate=2015-04-01&enddate=2015-07-04"
+            url = "https://hg.mozilla.org/%s/json-pushes?version=2&tipsonly=1&%s" % (branch, dateArgs)
+            try:
+                response = requests.get(url, headers={'accept-encoding':'json'}, verify=True)
+                data = response.json()
+            except:
+                print "   --------- failure getting json for url: %s" % url
+                return
+
+            # oldest to newest - I assume
+            for push in data['pushes']:
+                rev = data['pushes'][push]['changesets'][0][:12]
+                pushlog_cache[branch].append(rev)
+        savePushLogCache()
+
+
+def savePushLogCache():
+    global pushlog_cache
+    with open('pushlog.cache', 'wb') as fHandle:
+        json.dump(pushlog_cache, fHandle)
+
+
+
+
+# Given a revision get the push date from hg, in PDT+8
+def getRevisionDate(branch, rev):
+    global revision_cache
+    key = "%s-%s" % (branch, rev)
+    if key in revision_cache.keys():
+        return revision_cache[key]
+
+    if branch.startswith('firefox'):
+        branch = branch.replace('firefox', 'mozilla-central')
+
+    if branch.startswith('mozilla-aurora') or branch.startswith('mozilla-beta'):
+        br = "releases/%s" % branch.split('-non-pgo')[0]
+    elif not branch.startswith('mozilla-central'):
+        br = "integration/%s" % branch.split('-non-pgo')[0]
+    else:
+        br = "%s" % branch.split('-non-pgo')[0]
+
+    # https://hg.mozilla.org/integration/mozilla-inbound/json-pushes?changeset=b9150539d787&version=2&tipsonly=1
+    url = "https://hg.mozilla.org/%s/json-pushes?changeset=%s&version=2&tipsonly=1" % (br, rev) 
+    try:
+        response = requests.get(url, headers={'accept-encoding':'json'}, verify=True)
+        data = response.json()
+    except:
+        print "   --------- failure getting json for url: %s" % url
+        return ""
+
+    #TODO: how to deal with the timezones?
+    pushes = data['pushes'].keys()
+    date = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime(data['pushes'][pushes[0]]['date']))
+    revision_cache[key] = date
+    return date
+
 
 # ensure we have:
 #   branch, platform, test, percentage, date, revision
 
 def importPHData(filename):
-    phdata = []
+    phdata = {}
     branch = 0
     plat = 1
     test = 3
@@ -38,17 +134,13 @@ def importPHData(filename):
                     if row[test].endswith('opt'):
                         data[0] = "%s-non-pgo" % row[branch]
                     data[2] = row[test].split('summary')[0].strip()
-                    if row[test].split('summary')[0].strip().startswith('e10s'):
+                    if row[test].split('summary')[1].strip().startswith('e10s'):
                         data[1] = "%s-e10s" % row[plat]
-
-                if item == branch:
-                    if data[0].startswith('mozilla-central'):
-                        data[0] = data[0].replace('mozilla-central', 'firefox')
 
                 if item == pct:
                     # make percent x.xx%
                     #TODO: I assume this is regressions only
-                    data[3] = "-%.2f" % (float(row[pct]))
+                    data[3] = float("%.2f" % (abs(float(row[pct]))))
 
                 if item == date:
                     # date to human date
@@ -59,13 +151,21 @@ def importPHData(filename):
             if datetime.datetime.strptime(data[4], "%Y-%m-%d %H-%M-%S") < datetime.datetime.strptime('2015-03-30', "%Y-%m-%d"):
                 continue
 
-            #TODO: platform to e10s
-            phdata.append(data)
+            # TODO: this is hacky and depends on the data we are working with
+            if datetime.datetime.strptime(data[4], "%Y-%m-%d %H-%M-%S") > datetime.datetime.strptime('2015-07-02', "%Y-%m-%d"):
+                continue
+
+            if data[4].split(' ')[0] not in phdata.keys():
+                phdata[data[4].split(' ')[0]] = []
+            phdata[data[4].split(' ')[0]].append(data)
+            sorted(phdata[data[4].split(' ')[0]], key=lambda d: d[4])
+
     return phdata
 
-
 def importGSData(filename):
-    gsdata = []
+    loadRevisionCache()
+
+    gsdata = {}
     with open(filename, 'rb') as fHandle:
         alerts = csv.reader(fHandle, delimiter='\t')
         for row in alerts:
@@ -80,10 +180,34 @@ def importGSData(filename):
             data[4] = "%.2f" % (float(row[4].strip('%')))
 
             # TODO: this is hacky and depends on the data we are working with
-            if datetime.datetime.strptime(data[3], "%Y-%m-%d %H:%M:%S") > datetime.datetime.strptime('2015-07-02', "%Y-%m-%d"):
+            if datetime.datetime.strptime(data[3], "%Y-%m-%d %H:%M:%S") > datetime.datetime.strptime('2015-07-04', "%Y-%m-%d"):
                 continue
 
-            gsdata.append(data)
+            #TODO: we should support this!!!!
+            # we don't do anything for tp5n xperf bits
+            if data[2] == 'tp5n' or data[2] == 'xperf':
+                continue
+
+            # flip flop data 4 and 3
+            data[3] = abs(float(data[4]))
+            # get the actual push date
+            data[4] = getRevisionDate(data[0], data[5])
+
+            if data[0] == 'firefox':
+                data[0] = 'mozilla-central'
+            elif data[0] == 'firefox-non-pgo':
+                data[0] = 'mozilla-central-non-pgo'
+            elif data[0] not in ['fx-team', 'fx-team-non-pgo',
+                               'mozilla-inbound', 'mozilla-inbound-non-pgo',
+                               'b2g-inbound', 'b2g-inbound-non-pgo']:
+                continue
+
+            if data[4].split(' ')[0] not in gsdata.keys():
+                gsdata[data[4].split(' ')[0]] = []
+            gsdata[data[4].split(' ')[0]].append(data)
+            sorted(gsdata[data[4].split(' ')[0]], key=lambda d: d[4])
+
+    saveRevisionCache()
     return gsdata
 
 
@@ -134,46 +258,103 @@ def compareRevision(revision):
             print "  %s" % e
 
 
+def findMatch(target, haystack, rev):
+    for item in haystack:
+        if item[0] == target[0] and \
+           item[1] == target[1] and \
+           fuzzyRevisionMatch(target[0], target[5], item[5]) <= 10 and \
+           item[2] == target[2]:
+            return item
+    return None
 
+def fuzzyMatch(val1, val2):
+    diff = abs(float(val2) - float(val1))
+    # allow for a 10% difference
+    if float(val2) - float(val1) >= 0:
+        maxval = float(val2)
+    else:
+        maxval = float(val1)
+
+    if abs(maxval*.20) >= diff:
+        return True
+
+    return False
+
+def fuzzyRevisionMatch(branch, rev1, rev2):
+    global pushlog_cache
+
+    branch = branch.split('-non-pgo')[0]
+
+    if branch != 'mozilla-central':
+        branch = 'integration/%s' % branch
+
+    try:
+       index1 = pushlog_cache[branch].index(rev1)
+       index2 = pushlog_cache[branch].index(rev2)
+       # TODO: made this +- 3, the problem is we could coelesce,
+       # ideally this would be for revisions with data
+       return abs(index1 - index2)
+    except ValueError:
+       pass
+    return 100
+
+loadPushLogCache()
 phdata = importPHData('perfalerts.csv')
 gsdata = importGSData('alerts.csv')
 
+dates = set(phdata.keys()) | set(gsdata.keys())
+dates = sorted(dates)
 
-phrevisions = []
-for row in phdata:
-    if row[5] not in phrevisions:
-        phrevisions.append(row[5])
+for date in dates:
+    if date < '2015-06-18':
+        continue
 
-gsrevisions = []
-for row in gsdata:
-    if row[5] not in gsrevisions:
-        gsrevisions.append(row[5])
+    print "%s:" % date
+    if date in phdata and date in gsdata:
+        # minimum match: branch, platform, test, date
+        gsdata_extra = copy.deepcopy(gsdata[date])
+        for pitem in phdata[date]:
+            gitem = findMatch(pitem, gsdata[date], pitem[5])
+            if gitem:
+                try:
+                    gsdata_extra.remove(gitem)
+                except:
+                    print "failed to remove: %s" % gitem
 
-#TODO: derive revisions from phdata
-print "number of ph revisions: %s" % len(phrevisions)
-print "number of gs revisions: %s" % len(gsrevisions)
+                # match percent?
+                if gitem[3] == pitem[3] or fuzzyMatch(gitem[3], pitem[3]):
+                    # match revision?
+                    if gitem[5] == pitem[5] or fuzzyRevisionMatch(gitem[0], gitem[5], pitem[5]) <= 10:
+                        #perfect match: percent and revision
+#                        print "!! %s" % gitem
+                        pass
+                    else:
+                        print "## %s. g.%s != p.%s (%s pushes diff)" % (gitem, gitem[5], pitem[5], fuzzyRevisionMatch(gitem[0], gitem[5], pitem[5]))
+                        pass
+                elif gitem[5] == pitem[5] or fuzzyRevisionMatch(gitem[0], gitem[5], pitem[5]) <= 10:
+                    #fuzzy match - percent is off, but all other data is close
+                    print "$$ %s. g.%s != p.%s" % (gitem, gitem[3], pitem[3])
+                    pass
+                else:
+                    #minimum match
+                    print "@@ %s. g.%s != p.%s / g.%s != p.%s (%s rev diff)" % (gitem, gitem[3], pitem[3], gitem[5], pitem[5], fuzzyRevisionMatch(gitem[0], gitem[5], pitem[5]))
+            else:
+                #no match
+                print "p. %s." % (pitem)
+        for gitem in gsdata_extra:
+            print "g.%s." % (gitem)
 
-revisions = list(set(phrevisions) & set(gsrevisions))
-print "number of common revisions: %s" % len(revisions)
-extraph = list(set(gsrevisions) - set(revisions))
-print "number of unique ph revisions: %s" % len(extraph)
+    #TODO: deal with gdata only alerts
 
-revisions = []
-for r in extraph:
-    date = ''
-    for row in gsdata:
-        if row[5] == r:
-            date = row[3]
-            break
-    if not date:
-        print row
-    revisions.append(r)
-#    print "%s : %s" % (r, date)
+    """
+    if date in phdata
+        for item in phdata[date]:
+            print "-ph- %s" % item
+    if date in gsdata:
+        for item in gsdata[date]:
+            print "-gs- %s" % item
+    """
 
-    print "select branch, platform, test, percent, bug, status from alerts where keyrevision='%s';" % r
-#print revisions
-
-#revisions = ['b6623a27fa64']
 
 
 # store in: branch, platform, test, date, percent
