1fdc24fbc0be043a63478fb7cbb190a33f9b1df0
Joel Maher <joel.maher@gmail.com>
2019-01-16 11:40:16 -0500

    fixed linux64 platform, added filtering to compare opt/pgo

--- doalerts/perfherder_alerts.py
+++ doalerts/perfherder_alerts.py
@@ -1,11 +1,13 @@
 import json
 import requests
 import os
+import datetime
 
 from generate_alerts import RevisionDatum, detect_changes
 
 # constants in perfherder
 interval = 7776000 # 90 days
+interval = 31536000 # 1 year
 pgohash = 'f69e1b00908837bf0550250abb1645014317e8ec'
 thurl = 'https://treeherder.mozilla.org'
 
@@ -93,6 +95,16 @@ def filterSignatureIds(signatures, testname, subtests):
             sig_ids.append({'id': signatures[sig]['id'], 'metric': metric, 'option': option})
     return sig_ids
 
+
+def getTestNames(branch, framework, platform):
+    signatures = getSignatures(branch, framework, platform)
+    names = []
+    for sig in signatures:
+        if signatures[sig]['suite'] not in names:
+            names.append(signatures[sig]['suite'])
+    return names
+
+
 def analyzeData(sig, branch, framework):
     url = "https://treeherder.mozilla.org/api/project/%s/performance/data/?framework=%s&interval=%s&signature_id=%s"  % (branch, framework, interval, sig['id'])
     key = "%s-%s-%s" % (branch, framework, sig['id'])
@@ -106,39 +118,72 @@ def analyzeData(sig, branch, framework):
     return regressions
 
 
-def getTestNames(branch, framework, platform):
-    signatures = getSignatures(branch, framework, platform)
-    names = []
-    for sig in signatures:
-        if signatures[sig]['suite'] not in names:
-            names.append(signatures[sig]['suite'])
-    return names
+def filterUniqueAlerts(results):
+    filtered = [{'sig': {}, 'result': []},{'sig': {}, 'result': []}]
+    # TODO: consider weekends or slow times for a longer window - 24 hours
+    # TODO: sanity check no alerts are on the same 12 hour window (maybe within 100 pushids?)
+    hours = 6
+    first = [datetime.datetime.fromtimestamp(float(x.push_timestamp)) for x in results[0]['result']]
+    second = [datetime.datetime.fromtimestamp(float(x.push_timestamp)) for x in results[1]['result']]
+    f_matched = set()
+    s_matched = set()
+    for s in first:
+        f_matched.update([s for f in second if s < f + datetime.timedelta(hours=hours) and s > f - datetime.timedelta(hours=hours) and s not in list(f_matched)])
+    for s in second:
+        s_matched.update([s for f in first if s < f + datetime.timedelta(hours=hours) and s > f - datetime.timedelta(hours=hours) and s not in list(s_matched)])
+    sets = [list(set(first) - f_matched), list(set(second) - s_matched)]
+
+    for iter in [0,1]:
+        for item in results[iter]:
+            remaining = [x for x in results[iter]['result'] if datetime.datetime.fromtimestamp(float(x.push_timestamp)) in sets[iter]]
+            if len(remaining) == 0:
+                continue
+
+            filtered[iter]['sig'] = results[iter]['sig']
+            filtered[iter]['result'] = remaining
+        if filtered[iter] == {'sig': {}, 'result': []}:
+            filtered[iter] = None
+    results = [x for x in filtered if x != None]
+    return results
 
 
 def analyzeTest(framework, branch, platform, testname, subtests):
     signatures = getSignatures(branch, framework, platform)
     sig_ids = filterSignatureIds(signatures, testname, subtests)
 
+    results = []
     for sig in sig_ids:
-        regressions = analyzeData(sig, branch, framework)
-        print "%s: %s:%s" % (testname, sig['option'], sig['metric'])
-        for d in regressions:
-            print d
+        result = analyzeData(sig, branch, framework)
+        if result:
+            results.append({'sig': sig, 'result': result})
+
+    if results == []:
+        return
+
+    if not subtests and len(results) == 2:
+       results = filterUniqueAlerts(results)
+
+    for i in results:
+        print "%s: %s:%s (%s)" % (testname, i['sig']['option'], i['sig']['metric'], len(i['result']))
+        for d in i['result']:
+            date = datetime.datetime.fromtimestamp(float(d.push_timestamp)).isoformat()
+            print "%s (%s)" % (date, d.push_id)
+        print ""
 
 
 def main():
     # variables
     framework = getFrameworkId('raptor')
     branch = 'mozilla-inbound'
-    platform = 'windows10-64'
+    platforms = ['linux64', 'windows7-32', 'windows10-64']
     subtests = False
 
-    testnames = getTestNames(branch, framework, platform)
-    testnames = ['raptor-tp6-facebook-firefox']
+    for platform in platforms:
+        print "-------- %s -------------" % platform
+        testnames = getTestNames(branch, framework, platform)
 
-    for testname in testnames:
-        analyzeTest(framework, branch, platform, testname, subtests)
-        print ""
+        for testname in testnames:
+            analyzeTest(framework, branch, platform, testname, subtests)
 
 
 if __name__ == "__main__":
